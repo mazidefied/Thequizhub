d5b7da2d0cf16e1ce070a61b32099db6ac211744
George <gekere615@gmail.com>
2023-09-07 18:04:29 +0100

    Update QuizQuestion.jsx

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Options from './Options';
 
-const QuizQuestion = ({question, id, answerQuestion}) => {
-    const [selectedOption, setSelectedOption] = useState(null)
-    function handleAnswer(option){
-        setSelectedOption(option)
-        answerQuestion(id, option)
-    }
-    const options = question.options?question.options:[]
-    const optionsEl = options.map((option)=>{
-        return(<Options selectedOption={selectedOption} handleAnswer={handleAnswer} key={option} option={option} />)
-    })
+const QuizQuestion = ({ question, id, answerQuestion }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  function handleAnswer(option) {
+    setSelectedOption(option);
+    answerQuestion(id, option);
+  }
+
+  const options = question.options ? question.options : [];
+  const optionsEl = options.map((option) => {
+    return <Options selectedOption={selectedOption} handleAnswer={handleAnswer} key={option} option={option} />;
+  });
+
   return (
     <>
-        <div className="question">
-            <p>
-                {question.name}
-            </p>
-        </div>
-        <div className="options-holder">
-            {optionsEl}
-        </div>
+      <div className="question">
+        <p dangerouslySetInnerHTML={{ __html: question.name }} />
+      </div>
+      <div className="options-holder">{optionsEl}</div>
     </>
-  )
-}
+  );
+};
 
-export default QuizQuestion
+export default QuizQuestion;
